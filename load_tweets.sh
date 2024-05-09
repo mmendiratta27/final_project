4bb7927a1545ccd0a21bc356ba1f17ffd3814e50
Manan Mendiratta <123344066+mmendiratta27@users.noreply.github.com>
2024-05-09 12:42:15 -0700

    smaller for actions

--- load_tweets.sh
+++ load_tweets.sh
@@ -4,4 +4,4 @@ echo '==========================================================================
 echo 'loading tweets'
 echo '================================================================================'
 
-time python3 load_tweets.py --db=postgresql://postgres:pass@localhost:13231 --num_users=1000000 --num_tweets=10000000 --num_urls=1000000
+time python3 load_tweets.py --db=postgresql://postgres:pass@localhost:13231 --num_users=100 --num_tweets=100 --num_urls=100
